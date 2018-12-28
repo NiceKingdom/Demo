a416ac152afd5ca071fb0e50431d7abbb10dcf79
大狼狗 <UioSun@outlook.com>
2018-12-28 09:30:11 +0800

    Add progress bar eg.

--- frontend/src/components/plat.vue
+++ frontend/src/components/plat.vue
@@ -9,6 +9,13 @@
       <div class="item-12">12-cell</div>
       <div class="item-24">24-cell</div>
    </div>
+
+    <hr>
+
+    <button @click="test">点我改变长度</button>
+    <div class="progress">
+      <div class="loader-container" v-bind:style="styleObject"></div>
+    </div>
   </div>
 </template>
 
@@ -17,11 +24,46 @@
     name: 'plat',
     // 地图页面
     data () {
-      return {}
+      return {
+        styleObject: {
+          width: '89%'
+        },
+      }
     },
+    methods: {
+      test: function () {
+        this.styleObject.width = '12%'
+      },
+    }
   }
 </script>
 
+<style>
+  .progress{
+    width: 70%;
+    height: 7px;
+    border-radius:7px;
+    margin-left: 15%;
+    margin-top: 30%;
+    background: -webkit-linear-gradient(left,#e4e3e4,#e4e5e4);
+    background: -moz-linear-gradient(left,#e4e3e4,#e4e5e4);
+    background: -o-linear-gradient(left,#e4e3e4,#e4e5e4);
+    background: -ms-linear-gradient(left,#e4e3e4,#e4e5e4);
+    background: linear-gradient(left,#e4e3e4,#e4e5e4);
+  }
+  .loader-container {
+    height: 100%;
+    /*width: 30%;*/
+    background: -webkit-linear-gradient(left,#f8dc4b,#d95a3d);
+    background: -moz-linear-gradient(left,#f8dc4b,#d95a3d);
+    background: -o-linear-gradient(left,#f8dc4b,#d95a3d);
+    background: -ms-linear-gradient(left,#f8dc4b,#d95a3d);
+    background: linear-gradient(left,#f8dc4b,#d95a3d);
+    border-radius:7px;
+    box-shadow: inset 0 -2px 2px rgba(0, 0, 0, 0.2);
+    animation: loader 1s linear;
+  }
+</style>
 <style scoped lang="scss">
   @import "../assets/scss/mycss_lib";
   .test {
