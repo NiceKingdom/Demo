167cbb377cabf009d2cad8fc34dd17a51d8635a8
大狼狗 <UioSun@outlook.com>
2019-01-19 00:34:45 +0800

    发送空气质量提醒邮件到给定的用户 (#16)
    * Send Air Quality
    
    * Interim2
    
    * Upload get QA alert

---
+++ app/Mail/AirQualityWarning.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class AirQualityWarning extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $userInfo;
+
+    /**
+     * Create a new message instance.
+     *
+     * @param array $userInfo 用户信息
+     * @return void
+     */
+    public function __construct(array $userInfo)
+    {
+        $this->userInfo = $userInfo;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->from('UioSun@163.com', 'UioSun')
+            ->subject('空气质量小提醒')
+            ->view('emails.other.airQuality');
+    }
+}
--- app/Models/Log.php
+++ app/Models/Log.php
@@ -4,7 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Log
+ *
+ * @package Log
+ * @property string info 日志信息
+ * @property int status 状态码或错误码
+ * @property string category 日志类型
+ * @property string localization 发生位置
+ * @property int userId 用户 ID
+ * @property string uri URI
+ * @property string ip 用户 IP，仅供参考
+ */
 class Log extends Model
 {
-    //
+    public const DEFAULT_SUCCESS = 200;
+    public const DEFAULT_ERROR = 500;
+
+    public const CATEGORY = [
+        'AQ' => 'AirQuality',
+    ];
 }
